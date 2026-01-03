244075725069ffed4af2d27317f92b79ae1f7415
Ikramul Islam <mdikram295@gmail.com>
2026-01-03 16:33:03 +0600

    update build

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  project: {
+    android: {
+      packageName: 'com.connect.app',
+    },
+    ios: {
+      project: './ios/Connect.xcworkspace',
+    },
+  },
   dependencies: {
     // Disable native autolinking for the original package to avoid duplicate native classes
     'react-native-webview': {
