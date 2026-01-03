a5a85aec400c50f7997ec8eb0d55c9780c8e3927
Ikramul Islam <mdikram295@gmail.com>
2026-01-03 21:55:02 +0600

    Add Android cloud build workflow

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  project: {
-    android: {
-      packageName: 'com.connect.app',
-    },
-    ios: {
-      project: './ios/Connect.xcworkspace',
-    },
-  },
   dependencies: {
     // Disable native autolinking for the original package to avoid duplicate native classes
     'react-native-webview': {
